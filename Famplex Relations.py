5a427bf21c5f840f669c4eaec97986effd06a15a
KristianKolpeja <kkkolpeja@hotmail.it>
2018-07-30 15:14:27 +0200

    Update Famplex Relations

--- Famplex Relations.py
+++ Famplex Relations.py
@@ -1,42 +1,60 @@
-import pybel
-import urllib.request
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Convert famplex relations to BEL Statements."""
+
 import pandas as pd
-
-
-######Importing proteins and protein complexes######
-
-from pybel.dsl import protein, named_complex_abundance
+import pybel
 from pybel.constants import HAS_MEMBER
+from pybel.dsl import protein, named_complex_abundance
+
+FPLX = 'https://raw.githubusercontent.com/sorgerlab/famplex/master/relations.csv'
 
 
-######Accesing famplex relations file######
+def get_df():
+    """Get famplex relations as a Pandas dataframe.
 
-fplx = 'https://raw.githubusercontent.com/sorgerlab/famplex/master/relations.csv'
-df = pd.read_csv(fplx, header=None)
-
-
-######Representing relations as BEL statements######
-
-graph = pybel.BELGraph(name="Famplex", version="0.01", authors="Kristian Kolpeja")
-graph.namespace_url.update({
-    "HGNC": "https://arty.scai.fraunhofer.de/artifactory/bel/namespace/hgnc/hgnc-20180215.belns",
-    "FPLX": "https://raw.githubusercontent.com/sorgerlab/famplex/1b7e14ec0fd02ee7ed71514c6e267f57d5641a4b/export/famplex.belns"
-})
-
-for index, (ns1, n1, rel, ns2, n2) in df.iterrows():
-    if ns1 == "UP":
-        continue
-
-    sub = protein(ns1, n1)
-
-    if rel == 'isa':
-        obj = protein(ns2, n2)
-        graph.add_is_a(sub, obj)
-
-    else:  # partof
-        obj = named_complex_abundance(ns2, n2)
-        graph.add_unqualified_edge(obj, sub, HAS_MEMBER)
+    :rtype: pandas.DataFrame
+    """
+    return pd.read_csv(FPLX, header=None)
 
 
-with open("famplex.bel", "w") as file:
-    pybel.to_bel(graph, file)
+def build_graph(df):
+    """Build a BEL Graph from a famplex dataframe.
+
+    :param df: A Pandas dataframe representing famplex relations
+    :type df: pandas.DataFrame
+    :rtype: pybel.BELGraph
+    """
+    graph = pybel.BELGraph(name="Famplex", version="0.01", authors="Kristian Kolpeja")
+    graph.namespace_url.update({
+        "HGNC": "https://arty.scai.fraunhofer.de/artifactory/bel/namespace/hgnc/hgnc-20180215.belns",
+        "FPLX": "https://raw.githubusercontent.com/sorgerlab/famplex/1b7e14ec0fd02ee7ed71514c6e267f57d5641a4b/export/famplex.belns"
+    })
+
+    for index, (ns1, n1, rel, ns2, n2) in df.iterrows():
+        if ns1 == "UP":
+            continue
+
+        sub = protein(ns1, n1)
+
+        if rel == 'isa':
+            obj = protein(ns2, n2)
+            graph.add_is_a(sub, obj)
+
+        else:  # partof
+            obj = named_complex_abundance(ns2, n2)
+            graph.add_unqualified_edge(obj, sub, HAS_MEMBER)
+    return graph
+
+
+def main():
+    """Provide BEL Statements from BEL graph."""
+    df = get_df()
+    graph = build_graph(df)
+    with open("famplex.bel", "w") as file:
+        pybel.to_bel(graph, file)
+
+
+if __name__ == '__main__':
+    main()
